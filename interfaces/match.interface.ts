0d8bc80b868d9832ad81261ea2d90cb3017afe96
dudo48 <test>
2023-05-30 15:00:36 +0300

    added result to match interface

--- interfaces/match.interface.ts
+++ interfaces/match.interface.ts
@@ -1,8 +1,10 @@
+import MatchResult from './matchResult.interface';
 import Team from './team.interface';
 
 export default interface Match {
   id?: number;
   homeTeam: Team;
   awayTeam: Team;
+  result?: MatchResult;
   onNeutralGround: boolean;
 }
